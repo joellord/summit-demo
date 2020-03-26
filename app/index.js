329b6a3e0571896d0bf0cd59334c35c1e3772b0d
Joel Lord <joelphy@gmail.com>
2020-03-26 14:11:15 -0400

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/hello", (req, res) => {
+    res.send({value: "Hello"}).status(200);
+});
+
+app.get("/addtwo/:num", (req, res) => {
+    res.send({value: parseInt(req.params.num) + 2}).status(200);
+});
+
+app.get("/error", (req, res) => {
+    res.send("Error!").status(500);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening to port ${PORT}`);
+});
+
+module.exports = app;
---
+++ app/tests/integration.routes.test.js
@@ -0,0 +1,33 @@
+process.env.NODE_ENV = "test";
+
+const chai = require("chai");
+const should = chai.should();
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+const server = require("../");
+
+describe("GET /hello - hello world", () => {
+	it("should say hello", done => {
+		chai.request(server)
+		.get("/hello")
+		.end((err, res) => {
+			should.not.exist(err);
+			res.status.should.equal(200);
+			res.body.value.should.equal("Hello");
+			done();
+		});
+	});
+});
+
+describe("GET /addtwo - Additions", () => {
+	it("should add two to the parameter", done => {
+		chai.request(server)
+		.get("/addtwo/2")
+		.end((err, res) => {
+			should.not.exist(err);
+			res.status.should.equal(200);
+			res.body.value.should.equal(4);
+			done();
+		});
+	});
+});
