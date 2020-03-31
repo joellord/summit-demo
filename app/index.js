97e94eb12902c6826ae46c0ef8590706fe1f699c
Joel Lord <joelphy@gmail.com>
2020-03-31 16:02:10 -0400

    Added new pipeline

--- app/index.js
+++ app/index.js
@@ -1,22 +1,23 @@
 const express = require("express");
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
 app.get("/hello", (req, res) => {
-    res.send({value: "hello"}).status(200);
+  res.send({ value: "hello" }).status(200);
 });
 
 app.get("/addtwo/:num", (req, res) => {
-    res.send({value: parseInt(req.params.num) + 2}).status(200);
+  res.send({ value: parseInt(req.params.num) + 2 }).status(200);
 });
 
 app.get("/error", (req, res) => {
-    res.send("Error!").status(500);
+  res.send("Error!").status(500);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening to port ${PORT}`);
+  console.log(`Server listening to port ${PORT}`);
 });
 
 module.exports = app;
---
+++ start_pipeline.sh
@@ -0,0 +1,1 @@
+tkn pipeline start deploy-pipeline -r app-git=summit-demo-git -r app-image=summit-demo-image -s pipeline
